85a2f493a06f1ce4f235682a7d8b67add92a59df
양우석 <woosuk288@naver.com>
2021-01-10 19:40:34 +0900

    #14.4 React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./styles/styles.css";
 
 import { ApolloProvider } from '@apollo/client';
 import { client } from "./apollo";
+import LoggedOutRouter from './routers/logged-out-router';
 
 function App() {
   return (
     <ApolloProvider client={client}>
     <div className="bg-red-300">
       <h1 className="text-xl text-white">Hello Tailwind</h1>
+      <LoggedOutRouter />
     </div>
     </ApolloProvider>
   );
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoggedOutRouter() {
+  return (
+    <div>
+         Logged Out
+    </div>
+  )
+}
