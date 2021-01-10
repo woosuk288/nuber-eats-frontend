ba4e1781d0d7a987d49e897a97cd693595f1cf55
양우석 <woosuk288@naver.com>
2021-01-10 19:25:59 +0900

    #14.3 Apollo Setup
    - 프론트 포트 3000이라 백엔드 포트 4000으로 변경
    - Apollo Client Developer Tools 크롬 확장 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./styles/styles.css";
 
+import { ApolloProvider } from '@apollo/client';
+import { client } from "./apollo";
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="bg-red-300">
       <h1 className="text-xl text-white">Hello Tailwind</h1>
     </div>
+    </ApolloProvider>
   );
 }
 
