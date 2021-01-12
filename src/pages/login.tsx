17cb65159499ab1272c434b4ba2219b500f00173
양우석 <woosuk288@naver.com>
2021-01-12 16:24:05 +0900

    #15.5 Form Login
    - PostCSS 확장 설치하여 tailwind사용으로 인한 css 경고 제거.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,24 +1,50 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+
+  const onSubmit = () => {
+    console.log(getValues());
+  };
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
       <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
         <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form className="flex flex-col mt-5 px-5">
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 px-5">
           <input
+            ref={register({ required: "Email is required" })}
+            name="email"
             placeholder="Email"
             type="email"
-            className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
+            required
+            className="input"
           />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">{errors.email.message}</span>
+          )}
+
           <input
+            ref={register({ required: "Password is required", minLength: 10 })}
+            name="password"
             placeholder="Password"
             type="password"
-            className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
+            required
+            className="input"
           />
-          <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
-            Login
-          </button>
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">{errors.password.message}</span>
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="font-medium text-red-500">Password must be more than 10 chars.</span>
+          )}
+          <button className="btn">Login</button>
         </form>
       </div>
     </div>
