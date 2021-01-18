84ec61c45e8511af3045a7054d4780b5675fd07b
양우석 <woosuk288@naver.com>
2021-01-18 16:43:34 +0900

    #17.9 Restaurant part Two

--- src/pages/client/restaurant-detail.tsx
+++ src/pages/client/restaurant-detail.tsx
@@ -23,15 +23,29 @@ interface IRestaurantParams {
 
 export default function RestaurantDetail() {
   const { id } = useParams<IRestaurantParams>();
-  const { loading, data } = useQuery<restaurant, restaurantVariables>(RESTAURANT_QUERY, {
-    variables: {
-      input: {
-        restaurantId: +id,
+  const { loading, data: { restaurant } = {} } = useQuery<restaurant, restaurantVariables>(
+    RESTAURANT_QUERY,
+    {
+      variables: {
+        input: {
+          restaurantId: +id,
+        },
       },
-    },
-  });
+    }
+  );
 
-  console.log(loading, data);
-
-  return <div>RestaurantDetail Page</div>;
+  return (
+    <div>
+      <div
+        className="bg-red-500 py-24 bg-center bg-cover"
+        style={{ backgroundImage: `url(${restaurant?.restaurant?.coverImg})` }}
+      >
+        <div className="bg-white md:w-5/12 py-4 pl-5">
+          <h4 className="text-4xl mb-3">{restaurant?.restaurant?.name}</h4>
+          <h5 className="text-sm mb-2">{restaurant?.restaurant?.category?.name}</h5>
+          <h6 className="text-sm">{restaurant?.restaurant?.address}</h6>
+        </div>
+      </div>
+    </div>
+  );
 }
