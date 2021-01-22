f676531324e70ffaece5f7c2f2b95248fe519cb6
양우석 <woosuk288@naver.com>
2021-01-22 20:17:01 +0900

    #19.1 Our First Cypress Test
    - @testing-library/cypress 설치

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,5 +1,24 @@
-describe("First Test", () => {
-  it("should go to the hompage", () => {
-    cy.visit("http://localhost:3000").title().should("eq", "Login | Nuber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+
+  it("should fill out the form", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("client12345@naver.com")
+      .findByPlaceholderText(/password/i)
+      .type("12345")
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none");
+    // to do (can log in)
+  });
+
+  it("should see email and password validation errors", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("client@na")
+      .findByRole("alert")
+      .should("have.text", "Please enter a valid email");
   });
 });
